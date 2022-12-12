4b1fb46bee8098a0ff1bf66198b1598e2d6c5b18
Kangrui Wang <krwang2022@gmail.com>
2022-12-11 21:14:00 -0600

    Update align_response.py

--- align_response/code/align_response.py
+++ align_response/code/align_response.py
@@ -1,12 +1,11 @@
 import spacy
 import os
-spacy.prefer_gpu()
-nlp = spacy.load("en_core_web_sm")
 from utils import unzip
-
+import random
+import spacy
+import jsonlines
+import re
 def build_hypo_dictionary(read_path):
-    import jsonlines
-    import re
     dict_hypo_theoryset={}
     pattern = r'sent\d+: '
     with jsonlines.open(read_path, "r") as rfd:
@@ -25,11 +24,12 @@ def build_hypo_dictionary(read_path):
     return dict_hypo_theoryset,dict_hypo_theorypara
 
 def align_response(hypo_list,theory_list,i,j,nlp,involve_theory=False):
+    spacy.prefer_gpu()
+    nlp = spacy.load("en_core_web_sm")
     hypo1=hypo_list[i]
     hypo2=hypo_list[j]
     theory1=theory_list[i]
     theory2=theory_list[j]
-    import random
     
     #----------------------------------------------------------------------
     #build word_candidates
@@ -84,13 +84,13 @@ def align_response(hypo_list,theory_list,i,j,nlp,involve_theory=False):
         attackhypo+=chosen
     return attackhypo
 
-def show_sent(sent):
+def show_sent(sent,nlp):
     print(sent)
     doc = nlp(sent)
     for token in doc:
         print(token.text,token.pos_,token.dep_)
 
-def write_as_json(path):
+def write_as_json(hypo_list,theory_list,path,nlp):
     import random
     import jsonlines
     adversay_dict={}
@@ -111,9 +111,7 @@ def write_as_json(path):
 
 #write_as_json('test.jsonlines')     
 
-def write_as_json_all(hypo_list,theory_list,path):
-    import random
-    import jsonlines
+def write_as_json_all(hypo_list,theory_list,path,nlp):
     adversay_dict={}
     for i,hypo1 in enumerate(hypo_list):
         adversay_dict[hypo1]=""
@@ -128,23 +126,29 @@ def write_as_json_all(hypo_list,theory_list,path):
         for hypo,attack_list in adversay_dict.items():
             w.write({hypo: attack_list})
 
-
+def main():
+    tsk1_path="./data/entailment_trees_emnlp2021_data_v3/dataset/task_1"
+    train_path=os.path.join(tsk1_path,'train.jsonl')
+    spacy.prefer_gpu()
+    nlp = spacy.load("en_core_web_sm")
+    path='./data'
+    if not os.path.exists(path):
+        os.mkdir(path)
+    dict_set,dict_para=build_hypo_dictionary(train_path)
+    hypo_list=[]
+    theory_list=[]
+    for i,hypothesis1 in enumerate(dict_para.keys()):
+        hypo_list.append(hypothesis1)
+        theory_list.append(dict_para[hypothesis1])
+    write_as_json_all(hypo_list,theory_list,'./data/aligened_tree.jsonlines',nlp)
+    
 if __name__=='__main__':
     path='./data'
     if not os.path.exists(path):
         os.mkdir(path)
     #download_entailment_bank(path)
     zip_path="./data/entailment_trees_emnlp2021_data_v3.zip"
-    tsk1_path="./data/entailment_trees_emnlp2021_data_v3/dataset/task_1"
-    train_path=os.path.join(tsk1_path,'train.jsonl')
     unzip(zip_path,path)
-    # dict_set,dict_para=build_hypo_dictionary(train_path)
-    # hypo_list=[]
-    # theory_list=[]
-    # for i,hypothesis1 in enumerate(dict_para.keys()):
-    #     hypo_list.append(hypothesis1)
-    #     theory_list.append(dict_para[hypothesis1])
-    # write_as_json_all(hypo_list,theory_list,'./data/aligened_tree.jsonlines')
     zip_path="./data/aligened_tree.zip"
-    unzip_path="./data/aligened_tree.jsonlines"
+    unzip_path="./data/aligened_tree"
     unzip(zip_path,unzip_path)
