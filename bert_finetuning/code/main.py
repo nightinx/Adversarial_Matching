f8eff5894800148fe28e11a9cc45fe88e4ea0d78
Chameleon Cloud User <cc@ocl.novalocal>
2022-12-12 08:16:37 +0000

    qwe
    qwe
    :wq

--- bert_finetuning/code/main.py
+++ bert_finetuning/code/main.py
@@ -14,17 +14,17 @@ from modeling import BertForSeq
 from dataset import get_data,InputDataset
 import argparse
 import os
-
+from utils import set_seed
 def add_learner_params():
     parser=argparse.ArgumentParser()
     # trainer params
-    parser.add_argument('--train_data_size', default=100000, type=int, help='length of training data size')
+    parser.add_argument('--train_data_size', default=200000, type=int, help='length of training data size')
     parser.add_argument('--test_data_size', default=20000, type=int, help='length of test data size')
     parser.add_argument('--read_path1', default='./data/entailment_trees_emnlp2021_data_v3/dataset/task_1/train.jsonl', 
     type=str, help='read from entailment dataset')
     parser.add_argument('-read_path2', default='./data/aligened_tree/aligened_tree.jsonlines', type=str, help='read from aligned')
     parser.add_argument('--sent_len', default=500, type=int, help='max length of sentences fed into bert')
-    parser.add_argument('--batch_size', default=4, type=int, help='batch size for both training and eval')
+    parser.add_argument('--batch_size', default=16, type=int, help='batch size for both training and eval')
     parser.add_argument('--epochs', default=100, type=int, help='epoch for training')
     parser.add_argument('--save_dir', default='./bert_finetuning', type=str, help='save_path')
     args=parser.parse_args()
@@ -36,7 +36,7 @@ def add_learner_params():
 
 def train(args):
 
-    batch_size=args.batch_size,
+    batch_size=args.batch_size
     EPOCHS=args.epochs
     device=args.device
     #data=get_data(read_path1,read_path2)
@@ -45,8 +45,8 @@ def train(args):
     test_dataset=InputDataset(read_path1=args.read_path1,read_path2=args.read_path2,tokenizer=tokenizer,sent_len= args.sent_len,data_size= args.test_data_size,split=0.2,mode='test')
     model = BertForSeq.from_pretrained('bert-base-uncased')
     
-    train_dataloader = DataLoader(train_dataset,batch_size=4)
-    val_dataloader = DataLoader(test_dataset,batch_size=1)
+    train_dataloader = DataLoader(train_dataset,batch_size=batch_size)
+    val_dataloader = DataLoader(test_dataset,batch_size=batch_size)
 
     optimizer = AdamW(model.parameters(), lr=2e-5)
     total_steps = len(train_dataloader) * EPOCHS  # len(dataset)*epochs / batchsize
@@ -91,7 +91,7 @@ def train(args):
         log.info('Running Validation...')
 
         model.eval()
-        avg_val_loss, avg_val_acc = evaluate(model, val_dataloader)
+        avg_val_loss, avg_val_acc = evaluate(model, val_dataloader,args)
         val_time = format_time(time.time() - t0)
         log.info('====Epoch:[{}/{}] avg_val_loss={:.5f} avg_val_acc={:.5f}===='.format(epoch+1,EPOCHS,avg_val_loss,avg_val_acc))
         log.info('====Validation epoch took: {:}===='.format(val_time))
@@ -99,7 +99,7 @@ def train(args):
 
         if epoch == EPOCHS-1:
             savename=f"{time.strftime('%Y-%m-%d-%H-%M')}"
-            torch.save(model,os.path.join(args.save_dir,f'cache/model_{savename}.bin'))
+            torch.save(model.state_dict(),os.path.join(args.save_dir,f'cache/model_{savename}.bin'))
             print('Model Saved!')
     log.info('')
     log.info('   Training Completed!')
@@ -123,6 +123,8 @@ def evaluate(model,val_dataloader,args):
         ## gpu-> cpu
         preds = logits.detach().cpu().numpy()
         labels_ids = labels.to('cpu').numpy()
+        # print(preds)
+        # print(labels_ids)
         corrects.append((preds == labels_ids).mean())  ## [0.8,0.7,0.9]
         ## get loss
         loss = outputs.loss
@@ -168,5 +170,6 @@ def log_creater(output_dir):
     return log
 
 if __name__ == '__main__':
+    set_seed()
     args=add_learner_params()
     train(args)
